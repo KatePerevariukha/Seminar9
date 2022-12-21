f6b7062c79b874eefb856868927cffb4fc5fbd1e
Kate <Kate-pretty@ya.ru>
2022-12-21 14:46:04 +0500

    added files

---
+++ 66/Program.cs
@@ -0,0 +1,17 @@
+﻿int InputNum (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+int SumNbr (int m, int n)
+{
+    Console.WriteLine($"{m}, {n}");
+    if (m == n) return n;
+    else n += SumNbr (m, n-1);
+    return n;
+}
+
+int m = InputNum ("Введите число M: ");
+int n = InputNum ("Введите число N: ");
+Console.Write ($"Сумма чисел от {m} до {n} = {SumNbr (m, n)}");
